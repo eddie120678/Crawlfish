30469674ffe0edba23a4e2652da2f260e75832b0
Eddie Parks <eddie120678>
2020-08-09 20:20:49 -0600

    added control flow to user input

--- scraper.py
+++ scraper.py
@@ -1,14 +1,29 @@
 # this is a webcrawler
-import urllib2
+import urllib.request
+import urllib.parse
 
-pageOpener = urllib2.build_opener()
-pageOpener.addheaders = [{'User-agent', 'Mozilla/5.0'}]
 
-url = raw_input("Enter a web page: ")
 
-openURL = pageOpener.open(url).read()
 
-print(openURL)
-for word in openURL.split(' '):
-    if word.startswith('href'):
-        print(word)
+
+myHeader =  {'User-Agent':'Mozilla/5.0'}
+#ask user for web page if none given use default and if http forgotten it is appended
+def ask_user():
+    url = input("Enter a url for me to visit: ")
+    if url == "":
+        url = "http://phobos.ninja"
+    elif not url.startswith("http"):
+        url = "https://" + url
+        print("I added https:// for you, you lazy bastard!\n")
+    return url
+
+
+#basic get web page
+def get_page(page):
+
+    pageData = urllib.request.urlopen(page)
+    readData = pageData.read()
+    print(readData)
+
+url = ask_user()
+get_page(url)
