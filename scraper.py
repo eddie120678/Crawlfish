d97b111b0e3a05826064a0ba8ef8af7407f91688
Eddie Parks <eddie120678>
2020-08-19 10:19:46 -0600

    updated to include hardcoded pages that was previously overlooked

--- scraper.py
+++ scraper.py
@@ -7,6 +7,7 @@ import re
 keep_running = True
 url = ''
 
+
 def introduction():
     print("*"*24)
     print("* Welcome to Crawlfish *")
@@ -16,6 +17,7 @@ def introduction():
 
 myHeader =  {'User-Agent':'Mozilla/5.0'}
 #ask user for web page if none given use default and if http forgotten it is appended
+
 def ask_user():
 
 
@@ -37,8 +39,16 @@ def get_page(page):
     except(e):
         print(str(e))
 
+#function to display page stats
+def display_stats(linkn):
+    print("\n")
+    print("*" * 20)
+    print("I Have found " + str(linkn) + " links hardcoded and\n")
+    print("I found " + str(linkn) + " internal links from\n" + url)
+
 #find the links in data
 def find_links(my_data):
+    link_count = 0
     my_data = my_data.read()
     my_data = str(my_data, encoding = 'utf-8') #needed to split data otherwise it is bytecode
     #using regular expression to find all href in http document.
@@ -49,24 +59,33 @@ def find_links(my_data):
     '''
     
     for link in links:
+        if(link.startswith('http')):
+            print(link)
+            link_count = link_count + 1
         if(not link.startswith('http')):
             if(url.endswith('/') and not link.startswith('/')):
                 link = url + link
+                link_count = link_count + 1
                 print(link)
             elif(not url.endswith('/') and link.startswith('/')):
                 link = url + link
+                link_count = link_count + 1
                 print(link)
             elif(not url.endswith('/') and not link.startswith('/')):
                 link = url + '/' + link
+                link_count = link_count + 1
                 print(link)
             elif(url.endswith('/') and link.startswith('/')):
                 link = url + link.strip('/')
+                link_count = link_count + 1
                 print(link)
             else:
                 print("something went wrong you didn't account for idiot")
-
-
+        
+    return link_count
 '''
+    depricated function
+
     split_data = my_data.split()
     for word in split_data:
         if(word.startswith('href')):
@@ -77,7 +96,8 @@ introduction()
 while keep_running:
     url = ask_user()
     url_page = get_page(url)
-    find_links(url_page)
+    num_of_links = find_links(url_page)
+    display_stats(num_of_links)
     user_answer = input("Would you like to visit another page(y/n): ")
     if user_answer.lower() == "n":
         keep_running = False
